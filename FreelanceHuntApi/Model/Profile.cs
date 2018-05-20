3edf1564d16da91bffb40f814cbca27dc13d61c8
Іван Захарук <zaharuck@outlook.com>
2018-05-20 13:36:01 +0300

    Add project files.

---
+++ FreelanceHuntApi/Model/Profile.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace FreelanceHuntApi.Model
+{
+    class Profile
+    {
+        public string ProfileId { get; set; }
+
+
+        public string UrlAdress { get; set; }
+
+
+        public string UrlForPrivateMessage { get; set; }
+
+
+        public string Avatar { get; set; }
+
+
+        public bool IsFreelancer { get; set; }
+
+
+        public bool IsEmployer { get; set; }
+
+
+        public string Login { get; set; }
+
+
+        public string FirstName { get; set; }
+
+
+        public string Surname { get; set; }
+
+
+        public DateTime? BirthDate { get; set; }
+
+
+        public string Rating { get; set; }
+
+
+        public string RatingPosition { get; set; }
+
+
+        public string CountryName_ru { get; set; }
+
+
+        public string CityName_ru { get; set; }
+
+
+        public DateTime CreationDate { get; set; }
+
+
+        public string PositiveReviews { get; set; }
+
+
+        public string NegativeReviews { get; set; }
+
+
+        public string IsPhoneVerified { get; set; }
+
+
+        public string IsFirstNameVerified { get; set; }
+
+
+        public string IsBirthDateVerified { get; set; }
+
+
+        public string IsWmidVerified { get; set; }
+
+
+        public string IsOkpayVerified { get; set; }
+
+
+        public string IsEmailVerified { get; set; }
+
+
+        public bool IsOnline { get; set; }
+
+
+        public DateTime LastActivity { get; set; }
+
+
+        public string Website { get; set; }
+
+
+        public string StatusName { get; set; }
+
+
+        public string SkillName { get; set; }
+
+
+        public string SkillAltName { get; set; }
+
+
+        public string RatingPositionCategory { get; set; }
+
+
+        public string RatingPositionCategoryAlt { get; set; }
+
+
+        public string Email { get; set; }
+
+
+        public string Phone { get; set; }
+
+
+        public string Skype { get; set; }
+
+
+        public string Wmid { get; set; }
+
+
+        public string YandexMoney { get; set; }
+
+
+        public string Icq { get; set; }
+
+
+    }
+}
